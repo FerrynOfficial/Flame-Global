ca4bfed5739adeff4c4b158c1ac5d05e70c0e722
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-24 18:09:38 +0100

    Create say.js

---
+++ cmds/say.js
@@ -0,0 +1,16 @@
+exports.run = (client, message, args) => {
+    const Discord = require('discord.js')
+    const fs = require('fs')
+    
+    var text = message.content.split(' ').slice(1).join(' ')
+        if(!text) return message.reply('Укажите текст!')
+        var embed = new Discord.RichEmbed()
+        .setDescription(text)
+        .setColor('RANDOM')
+        message.channel.send(embed)
+        message.delete()
+       }      
+        exports.help = {
+            name:"say"
+       }
+        
