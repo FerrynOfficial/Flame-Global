c681e254fb3afa8d4f9d09b04ea6c62a4cda6f72
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-24 18:10:21 +0100

    Create warn.js

---
+++ cmds/warn.js
@@ -0,0 +1,47 @@
+exports.run = (client, message, args) => {
+    const Discord = require('discord.js')
+    const fs = require('fs')
+    const moment = require('moment')
+
+    if(!message.guild.me.permissions.has('MANAGE_MESSAGES')) {
+        message.reply('**У меня нет прав на это.**')
+        return
+    }
+    if(!message.member.permissions.has('MANAGE_MESSAGES')) {
+        message.reply('**У вас нет прав на это.**')
+        return
+    }
+    var warnmember = message.guild.member(message.mentions.users.first())
+    if(!warnmember) {
+        message.reply('**Пользователь не найден**\n`f!warn @User Причина`')
+        return
+    }
+    if(warnmember.permissions.has('MANAGE_MESSAGES')) {
+        message.reply('Данный пользователь имеет права равные вам.')
+        return
+    }
+    var reason = args.slice(1).join(' ')
+    if(!reason) {
+        message.reply('**Укажите причину.**')
+        return
+    }
+    var embed = new Discord.RichEmbed()
+    .setTitle('Предупреждение')
+    .setColor('FF0000')
+    .setDescription(`${warnmember} был предупрежден Модератором ${message.author}!\nПричина: **${reason}**`)
+    message.channel.send(embed)
+    var warnChannel = message.guild.channels.find(`name`, 'warns')
+    if(!warnChannel) return message.reply('Создайте канал под названием warns!')
+    var embed2 = new Discord.RichEmbed()
+    .setTitle('**Предупреждение | Logs**')
+    .setColor('FF0000')
+    .addField('Участник',`${warnmember}`)
+    .addField('Модератор',`${message.author}`)
+    .addField('Канал', message.channel)
+    .addField('Время',require('moment')(message.createdAt).format('HH:MM DD-MM-YY'), true)
+    .addField('Причина', `${reason}`)
+    warnChannel.send(embed2)
+}
+exports.help = {
+    name: 'warn'
+ }
