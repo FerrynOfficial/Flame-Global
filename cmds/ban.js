50404a16e04648d41a1a64cbd3a9de9850ea440f
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-24 18:06:23 +0100

    Create ban.js

---
+++ cmds/ban.js
@@ -0,0 +1,33 @@
+exports.run = (client, message, args) => {
+    if(!message.guild.me.permissions.has('BAN_MEMBERS')) {
+        message.reply('**У меня нет прав на это.**')
+        return
+    }
+    if(!message.member.permissions.has('BAN_MEMBERS')) {
+        message.reply('**У вас нет прав на это.**')
+        return
+    }
+    var banmember = message.guild.member(message.mentions.users.first())
+    if(!banmember) {
+        message.reply('**Пользователь не найден**\n`>ban @User Причина`')
+        return
+    }
+    if(banmember.permissions.has('BAN_MEMBERS')) {
+        message.reply('**Данный пользователь имеет права равные вам.**')
+        return
+    }
+    var reason = args.slice(1).join(' ')
+    if(!reason) {
+        message.reply('**Укажите причину.**')
+        return
+    }
+    message.guild.members.get(banmember.id).ban(reason)
+    var embed = new Discord.RichEmbed()
+    .setTitle('Бан')
+    .setDescription(`${banmember.user.tag} был забанен модератором ${message.author}!\nПричина: ${reason}`)
+    .setColor('FF0000')
+    message.channel.send(embed)
+}
+exports.help = {
+   name: 'ban'
+}
