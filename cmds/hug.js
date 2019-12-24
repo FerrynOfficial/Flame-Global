348b52481ea473678484434b57571acb02ee85e9
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-24 23:15:39 +0100

    Update hug.js

--- cmds/hug.js
+++ cmds/hug.js
@@ -1,12 +1,11 @@
 exports.run = (client, message, args) => {
     const Discord = require('discord.js')
-    var replies = ['https://tenor.com/view/hug-cuddle-comfort-love-friends-gif-5166500','https://tenor.com/view/hug-anime-gif-10195705', 'https://tenor.com/view/hug-anime-gif-11074788']
-    var result = Math.floor((Math.random() * replies.length));
     var hUser = message.mentions.users.first()
     if(!hUser) return message.reply('Укажите пользователя!')
     let embed = new Discord.RichEmbed()
-    .setTitle(`Пользователь обнял(а) ${hUser}!`)
-    .setImage(replies[result])
+    .setTitle(`Пользователь обнял(а) ${hUser.name}!`)
+    .setImage('https://tenor.com/view/hug-anime-gif-10195705')
+    .setColor('RANDOM')
     message.channel.send(embed)
 }
 exports.help = {
