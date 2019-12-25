359f5ebb951d0b554f7a39f6e2bf6432df177e39
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-25 12:35:04 +0100

    Update slap.js

--- cmds/slap.js
+++ cmds/slap.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
     var sUser = message.guild.member(message.mentions.users.first())
     if(!sUser) return message.reply('Укажите пользователя которого хотите шлепнуть!')
     let embed = new Discord.RichEmbed()
-    .setTitle(`**Вы шлепнули ${sUser}**!`)
+    .setDescription(`**Вы шлепнули ${sUser}**!`)
     .setImage('https://media.discordapp.net/attachments/653184526356447232/659355933037690910/slapanimefine.gif')
     .setColor('RANDOM')
     message.channel.send(embed)
