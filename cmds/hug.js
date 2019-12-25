7984bb033ec551eb6d7997f715004f9c078d86f3
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-25 10:58:56 +0100

    Update hug.js

--- cmds/hug.js
+++ cmds/hug.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
     var hUser = message.guild.member(message.mentions.users.first())
     if(!hUser) return message.reply('Укажите пользователя которого хотите обнять!')
     let embed = new Discord.RichEmbed()
-    .setTitle(`**Вы обняли ${hUser}**!`)
+    .setDescription(`**Вы обняли ${hUser}**!`)
     .setImage('https://cdn.discordapp.com/attachments/653184526356447232/659332731607449615/tenor_5.gif')
     .setColor('RANDOM')
     message.channel.send(embed)
