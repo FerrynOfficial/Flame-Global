9555cce0465f7840d50945e07b460dd74f3c63ab
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-26 13:42:18 +0100

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
     const Discord = require('discord.js')
     const fs = require('fs')
     const moment = require('moment')
-    var funcmds = ['`coin` - Подбрасывает монетку', '`random` - Случайное число от 1 до 10', '`8ball` - Дает ответ на любой вопрос ','`iq` - Показывает ваш IQ', '`say` - Отправляет сообщение от лица бота']
+    var funcmds = ['`hug`- Обнять пользователя','`kiss`- Поцеловать пользователя','`slap`- Шлепнуть пользователя','`coin` - Подбрасывает монетку', '`random` - Случайное число от 1 до 10', '`8ball` - Дает ответ на любой вопрос ','`iq` - Показывает ваш IQ', '`say` - Отправляет сообщение от лица бота']
     var modcmds = ['`warn` - Выдает предупреждение', '`kick` - Кикает участника', '`ban` - Банит участника', '`purge` - Очищает чат', '`mute` - Выдает мут', '`dm` - Отправляет личное сообщение участнику','`addRole` - Выдать роль участнику', '`removeRole` - Забрать роль у участника']
     var cmds = ['`avatar` - Показать аватар участника', '`ping` - Показывает пинг бота', '`poll` - Проводит голосование', '`serverinfo` - Показывает информацию о сервере', '`about` - Показывает информацию о боте', '`userinfo`- Получить информацию о участнике']
     var embed = new Discord.RichEmbed()
@@ -13,7 +13,7 @@ exports.run = (client, message, args) => {
     .addBlankField()
     .addField("**Техническая Поддержка**","[Нажмите сюда](https://discord.gg/TszUh32)")
     .addBlankField()
-    .addField("**Пригласить бота на сервер**", "[Нажмите сюда](https://discordapp.com/oauth2/authorize?client_id=659082682118045716&scope=bot&permissions=8)")
+    .addField("**Пригласить бота на сервер**", "[Нажмите сюда](https://discordapp.com/login?redirect_to=%2Foauth2%2Fauthorize%3Fclient_id%3D659039854834745344%26scope%3Dbot%26permissions%3D8)")
     .setFooter('В скором времени будет добавлено больше команд!')
     message.member.send(embed)
     var embed2 = new Discord.RichEmbed()
