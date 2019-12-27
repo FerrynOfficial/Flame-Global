1ff134e32b0a210ae5474319b79135cea99008c6
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-27 19:40:23 +0100

    Update unmute.js

--- cmds/unmute.js
+++ cmds/unmute.js
@@ -6,15 +6,14 @@ exports.run = async(client, message, args) => {
     if(!muser.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.reply('Данный пользователь имеет права равные Вам!')
     if(!muser.highestRole.calculatedPosition >= message.guild.me.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.reply('У меня нет прав!')
     var muterole = message.guild.roles.find(`name`, 'Muted')
-    if(!muterole && muser.roles.has(muterole.id)) 
-    var embed = new Discord.RichEmbed()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription(`**Пользователь ${muser} был размучен!**`)
-    .addField('**Модератор**', message.author, true)
-    message.channel.send(embed)
+    embed.setAuthor(message.author.username, message.author.avatarURL)
+    embed.setDescription(`**Пользователь ${muser} был размучен!**`)
+    embed.addField('**Модератор**', message.author, true)
+    embed.setColor('006400')
+    message.channel.send()
     await muser.removeRole(muterole)
 }
-exports.help = {
-    name: 'unmute'
+    exports.help = {
+        name: 'unmute'
+    }
 
-}
