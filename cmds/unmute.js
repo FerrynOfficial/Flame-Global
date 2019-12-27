a9b9c931b04e75ab7eaed2ada0ade6114c8716b6
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-27 19:10:50 +0100

    Update unmute.js

--- cmds/unmute.js
+++ cmds/unmute.js
@@ -7,20 +7,12 @@ exports.run = async(client, message, args) => {
     if(!muser.highestRole.calculatedPosition >= message.guild.me.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.reply('У меня нет прав!')
     var muterole = message.guild.roles.find(`name`, 'Muted')
     if(!muterole && muser.roles.has(muterole.id)) 
-    var logschannel = message.guild.channels.find(`name`, 'logs')
-    if(!logschannel) return message.reply('Создайте канал с названием "logs"!')
     let embed = new Discord.RichEmbed()
     .setAuthor(message.author.username, message.author.avatarURL)
     .setDescription(`**Пользователь ${muser} был размучен!**`)
     .addField('**Модератор**', message.author, true)
     message.channel.send(embed)
     await muser.removeRole(muterole)
-    let embed1 = new Discord.RichEmbed()
-    .setTitle('Unmute | Logs')
-    .addField('Пользователь', muser, true)
-    .addField('Модератор', message.author, true)
-    .addField('Канал', message.channel, true)
-    logschannel.send(embed1)
 }
 exports.help = {
     name: 'unmute'
