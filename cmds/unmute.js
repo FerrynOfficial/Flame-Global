d727f699bd79bbacec8eaf1a87b91b9f9f47d332
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-28 10:59:24 +0100

    Update unmute.js

--- cmds/unmute.js
+++ cmds/unmute.js
@@ -7,6 +7,8 @@ exports.run = async(client, message, args) => {
     if(!muser.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.reply('Данный пользователь имеет права равные Вам!')
     if(!muser.highestRole.calculatedPosition >= message.guild.me.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.reply('У меня нет прав!')
     var muterole = message.guild.roles.find(`name`, 'Muted')
+    var logs = message.guild.channels.find(`name`, 'logs')
+    if (!logs) return message.reply('Создайте канал под названием "logs"!')
     embed.setAuthor(message.author.username, message.author.avatarURL)
     embed.setDescription(`**Пользователь ${muser} был размучен!**`)
     embed.addField('**Модератор**', message.author, true)
