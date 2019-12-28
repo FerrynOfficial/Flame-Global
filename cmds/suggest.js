5504e886d0d5ed1ac01bec49acdcf642e6877ed7
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-28 13:49:03 +0100

    Update suggest.js

--- cmds/suggest.js
+++ cmds/suggest.js
@@ -2,8 +2,9 @@ exports.run = async(client, message, args) => {
     const Discord = require('discord.js')
     var embed = new Discord.RichEmbed()
     client.owner = '525003205394825257'
+    var owner = client.owner
 
-    var suggest = args.slice(1).join(' ')
+    var suggest = args.slice(0).join(' ')
     if (!suggest) {
         message.channel.send('Укажите идею, которую хотите предложить.')
         return
@@ -12,7 +13,7 @@ exports.run = async(client, message, args) => {
     embed.setDescription(suggest)
     embed.setColor('RANDOM')
     embed.addField('Автор идеи', message.author, true)
-    client.owner.send(embed)
+    owner.send(embed)
 }
 exports.help = {
     name: 'suggest'
